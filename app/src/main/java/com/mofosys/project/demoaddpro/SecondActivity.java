bcde6293c9347d77d23b596132d74e76cab7f4de
Girish <girishby9@gmail.com>
2017-08-03 13:10:19 +0530

    first commit

---
+++ app/src/main/java/com/mofosys/project/demoaddpro/SecondActivity.java
@@ -0,0 +1,48 @@
+package com.mofosys.project.demoaddpro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.AdRequest;
+
+/**
+ * Created by girish on 22/6/17.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    private String TAG = SecondActivity.class.getSimpleName();
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        mInterstitialAd = new InterstitialAd(this);
+
+        // set the ad unit ID
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_full_screen));
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+
+        // Load ads into Interstitial Ads
+        mInterstitialAd.loadAd(adRequest);
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                showInterstitial();
+            }
+        });
+    }
+
+    private void showInterstitial() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
+    }
+
+}
